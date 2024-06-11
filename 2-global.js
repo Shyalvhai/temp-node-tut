9db46ab07e2873d18e6be67b1170f6476806035e
Avishek Prajapati <avishekprajapati@MacBook-Pro.local>
2024-06-11 12:23:57 +1000

    first commit

---
+++ 2-global.js
@@ -0,0 +1,13 @@
+// GLobals - No Window !!!]
+
+// __dirname - path to current directory
+// __filename - filename
+// require - function to use modules (common.js)
+// module - info about current modules (file)
+// process - info about the env where the program is being executed
+
+console.log(__dirname)
+
+SetInterval(()=>{
+    console.log("hello world")
+}, 1000)
